180ca4c0f0a2c0b172adadf06e291b9409af8408
Israel Torres <israeltorres@uadec.edu.mx>
2019-09-19 20:10:44 -0500

    PrimerCambio

---
+++ pagina2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagina2</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="divs"></div>
+    <div class="divs"></div>
+    <div class="divs"></div>
+    <div class="divs"></div>
+    <div class="divs"></div>
+</body>
+</html>
